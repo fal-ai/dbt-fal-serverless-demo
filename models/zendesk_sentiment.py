24fc8982c0473c7cd9df9975bbada3f6b3c2347c
Meder Kamalov <meder@kamalov.net>
2023-03-30 19:44:37 +0200

    Update Python model to work on GPU

--- models/zendesk_sentiment.py
+++ models/zendesk_sentiment.py
@@ -1,17 +1,33 @@
 def model(dbt, fal):
     dbt.config(materialized="table")
-    dbt.config(fal_environment="funny", fal_machine="GPU-T4")
-    from transformers import pipeline
+    dbt.config(fal_environment="sentiment-analysis")
+    dbt.config(fal_machine="GPU-T4")
+    from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
     import numpy as np
     import pandas as pd
+    import torch
+
+    # Check if a GPU is available and set the device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print(f"Running sentiment analysis on: {device}")
+
+    # Load the model and tokenizer
+    model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+    model = AutoModelForSequenceClassification.from_pretrained(model_name).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # Create the sentiment-analysis pipeline
+    classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
     ticket_data = dbt.ref("zendesk_ticket_data")
-    ticket_descriptions = list(ticket_data.description)
-    classifier = pipeline("sentiment-analysis", accelerator="cuda")
-    description_sentimet_analysis = classifier(ticket_descriptions)
+    ticket_descriptions = ticket_data["DESCRIPTION"].tolist()
+
+    # Run the sentiment analysis on the ticket descriptions
+    description_sentiment_analysis = classifier(ticket_descriptions)
     rows = []
 
-    for id, sentiment in zip(ticket_data.id, description_sentimet_analysis):
+    for id, sentiment in zip(ticket_data.ID, description_sentiment_analysis):
         rows.append((int(id), sentiment["label"], sentiment["score"]))
 
     records = np.array(rows, dtype=[("id", int), ("label", "U8"), ("score", float)])
