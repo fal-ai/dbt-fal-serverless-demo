d31b329987d31f2ceb836e996d4840d397730793
Meder Kamalov <meder@kamalov.net>
2023-03-30 20:14:01 +0200

    Fix python model

--- models/zendesk_sentiment.py
+++ models/zendesk_sentiment.py
@@ -1,24 +1,21 @@
 def model(dbt, fal):
     dbt.config(materialized="table")
     dbt.config(fal_environment="sentiment-analysis")
-    dbt.config(fal_machine="GPU-T4")
+    dbt.config(fal_machine="GPU")
     from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
     import numpy as np
     import pandas as pd
     import torch
 
-    # Check if a GPU is available and set the device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    print(f"Running sentiment analysis on: {device}")
+    # Check if a GPU is available and set the device index
+    device_index = 0 if torch.cuda.is_available() else -1
 
     # Load the model and tokenizer
     model_name = "distilbert-base-uncased-finetuned-sst-2-english"
-    model = AutoModelForSequenceClassification.from_pretrained(model_name).to(device)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # Create the sentiment-analysis pipeline
-    classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    # Create the sentiment-analysis pipeline with the specified device
+    classifier = pipeline("sentiment-analysis", model=model_name, tokenizer=tokenizer, device=device_index)
 
     ticket_data = dbt.ref("zendesk_ticket_data")
     ticket_descriptions = ticket_data["DESCRIPTION"].tolist()
